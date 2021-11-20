93826113b4e94c4f57452c6fa1d1ad5a82c56661
fongziyjun16 <fongziyjun16@hotmail.com>
2021-11-20 12:08:42 -0500

    update

--- LinesOfCode/main.py
+++ LinesOfCode/main.py
@@ -1,8 +1,4 @@
 import os
 
 if __name__ == '__main__':
-    files = []
-    for file in os.listdir("E:/Documents/Courses/DOSP/Projs/Proj04/PartOne/demo01"):
-        if file.endswith(".fs"):
-            files.append(file)
     pass
