caad1286661f2cd6712a94ea1a3f33ceb1d526bb
fongziyjun16 <fongziyjun16@hotmail.com>
2021-11-23 12:56:51 -0500

    update

---
+++ LinesOfCode/README.md
@@ -0,0 +1,5 @@
+## Files Line Counter
+
+- Given a PATH of project directory
+
+- Calculate the total number of all specific files (like *.java*, *.cs*, *.cpp*)
--- LinesOfCode/main.py
+++ LinesOfCode/main.py
@@ -1,4 +1,36 @@
 import os
 
+
+class FileLineCounter:
+    __totalLine = 0
+
+    @classmethod
+    def __count_file(cls, file_path):
+        count = 0
+        file = open(file_path)
+        for line in file:
+            if line != "\n":
+                count += 1
+                cls.__totalLine += 1
+        file.close()
+        print(file_path + ": " + str(count))
+
+    @classmethod
+    def __count_dir(cls, dir_path):
+        items = os.scandir(dir_path)
+        for item in items:
+            item_path = dir_path + "/" + item.name
+            if os.path.isfile(item_path) and item_path[-3:] == ".fs":
+                cls.__count_file(item_path)
+            elif os.path.isdir(item_path):
+                cls.__count_dir(item_path)
+
+    @classmethod
+    def count(cls, dir_path):
+        cls.__count_dir(dir_path)
+        return cls.__totalLine
+
+
 if __name__ == '__main__':
-    pass
+    dirPath = "E:/Documents/Courses/DOSP/Projs/Proj03/ChordP2PSystemSimulator"
+    print("total: " + str(FileLineCounter.count(dirPath)))
